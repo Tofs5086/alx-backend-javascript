aa6c4232721ad2a83843ddeaccc50d160d5105dd
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2024-07-22 19:33:29 +0100

    fix2

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,9 +1,5 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable */
-
-
 export function taskFirst() {
-  const task = 'I prefer const when I can.';
+  var task = 'I prefer const when I can.';
   return task;
 }
 
@@ -12,7 +8,7 @@ export function getLast() {
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let';
+  var combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
