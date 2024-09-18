f645b7f2f8c61710e32b81afddbe87dd0649cb45
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2024-09-18 18:41:21 +0100

    console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author tofs5086 <https://github.com/tofs5086>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
